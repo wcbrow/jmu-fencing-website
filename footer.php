3d29174dddfd8df79aa13c7cda471466f5d33be6
bradlech <cole.hamilton.bradley@gmail.com>
2014-05-01 18:02:08 -0400

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<div id="footer">
-	All content &copy; <?php echo date('Y'); ?> JMU Fencing Club. | <a href="<?php echo SITEURL . '/admin/'; ?>">Admin Area</a>
-	<?php if(logged_in()) { echo ' | <a href="' . SITEURL . '/logout.php">Logout</a>'; } ?>
-</div>
-</body>
-</html>
