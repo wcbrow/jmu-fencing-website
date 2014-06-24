ae25b90264d5f383bb4bfd5ec6d8b51f46200c82
wcbrow <wcbrow@gmail.com>
2014-06-24 14:36:46 -0400

    Create index.php
    This is just a skeleton of the homepage.  Most of the sections will likely wind up being developed as separate pages and included in the final version for potential reusability, but we'll see.
    
    The meta and any other scripting is currently blank until we get a little further along and know exactly what we want to do, and how we want to present ourselves.

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<meta charset="UTF-8">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
+		<meta name="author" content="">
+		<meta name="keywords" content="">
+		<link type="text/css" rel="stylesheet" src="fencing.css">
+		<script></script>
+	</head>
+	<body>
+		<header>
+			<img id="logo">
+		</header>
+		<nav>
+			<a href="">About Fencing</a>
+			<a href="">About the Club</a>
+			<a href="">Constitution</a>
+			<a href="">Member Login</a>
+			<a href="">Contact Us</a>
+			<a href="">Officers</a>
+		</nav>
+		<main>
+			<section id="slideshow"></section>
+			<section id="news"></section>
+			<section id="events"></section>
+
+			<!--   This is the starter for any other section we might want to add
+						ex. ads, testimonials, ladder cutoff, some form of ticker, social media, etc.
+
+			<section id="practice"></section>
+			<section id="sample"></section>
+
+			-->
+		</main>
+		<footer></footer>
+	</body>
+</html>
